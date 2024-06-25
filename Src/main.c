4d3a01d555643dd1a4466159997dd6b360a0d846
Joel Alejandro Milla Lopez <146555217+Joel-Milla@users.noreply.github.com>
2024-06-25 17:53:19 -0600

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -36,31 +36,31 @@ uint8_t getCharacter(uint8_t, uint8_t);
 
 int main(void)
 {
-	RCC_AHB1ENR_t volatile *const pAHB1ClockEnableRegister = (RCC_AHB1ENR_t*) 0x40023830; // Memory address of RCC to enable the clock
-	GPIOx_MODE_t volatile *const pGPIODPortModeRegister = (GPIOx_MODE_t*) 0x40020C00; // Memory address to change the port modes (input/output/etc) of the pins in GPIOD
-	GPIOx_ODR_t volatile *const pGPIODOutputDataRegister = (GPIOx_ODR_t*) 0x40020C14; // Memory address to change the output of pins
-	GPIOx_IDR_t volatile *const pGPIODInputDataRegister = (GPIOx_IDR_t*) 0x40020C10; // Memory address to read if there is any input in the pins
-	GPIOx_PUPDR_t volatile *const pGPIODPullRegister = (GPIOx_PUPDR_t*) 0x40020C0C; // Memory address to change to pull up/down registers on specific pins
+	RCC_AHB1ENR_t volatile *const pAHB1ClockEnableRegister = ADDR_REG_AHB1ENR; // Memory address of RCC to enable the clock
+	GPIOx_MODE_t volatile *const pGPIODPortModeRegister = ADDR_REG_GPIOD_MODE; // Memory address to change the port modes (input/output/etc) of the pins in GPIOD
+	GPIOx_ODR_t volatile *const pGPIODOutputDataRegister = ADDR_REG_GPIOD_OUTPUT_DATA; // Memory address to change the output of pins
+	GPIOx_IDR_t volatile *const pGPIODInputDataRegister = ADDR_REG_GPIOD_INPUT_DATA; // Memory address to read if there is any input in the pins
+	GPIOx_PUPDR_t volatile *const pGPIODPullRegister = ADDR_REG_RESISTOR; // Memory address to change to pull up/down registers on specific pins
 
 	// 2. Initialize the clock register for the port that has the pins that will be used (GPIOD)
-	pAHB1ClockEnableRegister->gpiod_en = 1;
+	pAHB1ClockEnableRegister->gpiod_en = CLOCK_ENABLE;
 
 	// 3. Initialization of variables and setup of environment for the MCU
 	// a. Make all row IOs mode as output (PD0-PD3)
-	pGPIODPortModeRegister->pin_0 = 1;
-	pGPIODPortModeRegister->pin_1 = 1;
-	pGPIODPortModeRegister->pin_2 = 1;
-	pGPIODPortModeRegister->pin_3 = 1;
+	pGPIODPortModeRegister->pin_0 = MODE_CONF_OUTPUT;
+	pGPIODPortModeRegister->pin_1 = MODE_CONF_OUTPUT;
+	pGPIODPortModeRegister->pin_2 = MODE_CONF_OUTPUT;
+	pGPIODPortModeRegister->pin_3 = MODE_CONF_OUTPUT;
 	// b. Make all column IOs mode as input (PD8-PD11)
-	pGPIODPortModeRegister->pin_8 = 0;
-	pGPIODPortModeRegister->pin_9 = 0;
-	pGPIODPortModeRegister->pin_10 = 0;
-	pGPIODPortModeRegister->pin_11 = 0;
+	pGPIODPortModeRegister->pin_8 = MODE_CONF_INPUT;
+	pGPIODPortModeRegister->pin_9 = MODE_CONF_INPUT;
+	pGPIODPortModeRegister->pin_10 = MODE_CONF_INPUT;
+	pGPIODPortModeRegister->pin_11 = MODE_CONF_INPUT;
 	// c. Activate internal pull-up resistors for columns IOs
-	pGPIODPullRegister->pin_8 = 1;
-	pGPIODPullRegister->pin_9 = 1;
-	pGPIODPullRegister->pin_10 = 1;
-	pGPIODPullRegister->pin_11 = 1;
+	pGPIODPullRegister->pin_8 = MODE_CONF_PULLUP_RESISTORS;
+	pGPIODPullRegister->pin_9 = MODE_CONF_PULLUP_RESISTORS;
+	pGPIODPullRegister->pin_10 = MODE_CONF_PULLUP_RESISTORS;
+	pGPIODPullRegister->pin_11 = MODE_CONF_PULLUP_RESISTORS;
 
 	// 4. Implement the key detect logic
 	while (1) {
@@ -72,16 +72,16 @@ int main(void)
 			// b. Set Row_i (i = pinIterator) as low
 			switch(pinIterator) {
 			case 0:
-				pGPIODOutputDataRegister->pin_0 = 0;
+				pGPIODOutputDataRegister->pin_0 = SET_ROW_LOW;
 				break;
 			case 1:
-				pGPIODOutputDataRegister->pin_1 = 0;
+				pGPIODOutputDataRegister->pin_1 = SET_ROW_LOW;
 				break;
 			case 2:
-				pGPIODOutputDataRegister->pin_2 = 0;
+				pGPIODOutputDataRegister->pin_2 = SET_ROW_LOW;
 				break;
 			case 3:
-				pGPIODOutputDataRegister->pin_3 = 0;
+				pGPIODOutputDataRegister->pin_3 = SET_ROW_LOW;
 				break;
 			}
 			// c. Check what column changed to zero (C1-C4) - To confirm if those are the pressed key
@@ -95,17 +95,17 @@ int main(void)
 // This function sets all the rows output state as high (as 1) so the columns are also shown all as high
 void setRowsAsHigh(GPIOx_ODR_t volatile *const pGPIODOutputDataRegister)
 {
-	pGPIODOutputDataRegister->pin_0 = 1;
-	pGPIODOutputDataRegister->pin_1 = 1;
-	pGPIODOutputDataRegister->pin_2 = 1;
-	pGPIODOutputDataRegister->pin_3 = 1;
+	pGPIODOutputDataRegister->pin_0 = SET_ROW_HIGH;
+	pGPIODOutputDataRegister->pin_1 = SET_ROW_HIGH;
+	pGPIODOutputDataRegister->pin_2 = SET_ROW_HIGH;
+	pGPIODOutputDataRegister->pin_3 = SET_ROW_HIGH;
 }
 
 // Function that makes a delay when printing the values
 void delay()
 {
 	// Make a delay using a for loop
-	for(uint32_t volatile iterator; iterator < 300000; iterator++); // Causes 150miliseconds = 0.15 seconds
+	for(uint32_t volatile iterator; iterator < DELAY_COUNT; iterator++); // Causes 150miliseconds = 0.15 seconds
 }
 
 
@@ -174,8 +174,3 @@ void findConnectionCx(uint8_t row, GPIOx_IDR_t volatile *const pGPIODInputDataRe
 		printf("%c\n", getCharacter(row, columnOn));
 	}
 }
-
-
-
-
-
